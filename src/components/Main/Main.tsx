8f5ab1d680ed4f37622901ded58952d1ddf25dd8
Angelika Makarova <esipovaangelika@mail.ru>
2023-04-07 01:53:49 +0300

    Style the form for creating a room

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -31,7 +31,7 @@ export function Main() {
                 <div className="flex flex-col items-center justify-center h-1/2">
                     <h2 className="text-left w-4/5 sm:w-1/3 font-bold mb-6 text-2xl">Register</h2>
                     <input type='text' value={name} placeholder='Name' onChange={handleNameChange}
-                        className="w-4/5 sm:w-1/3 border-2 border-gray-200 focus:outline-indigo-500 focus:border-0 rounded-md p-3 mb-4" />
+                        className="w-4/5 sm:w-1/3 border-2 border-gray-200 focus:outline-indigo-500 rounded-md p-3 mb-4" />
                     <button onClick={signUp}
                     className="w-4/5 sm:w-1/3 font-bold bg-indigo-500 hover:opacity-50 ease-in-out duration-200 rounded-md text-slate-50 p-3">Sign up</button>
                 </div>
--- src/components/NewRoomForm/NewRoomForm.tsx
+++ src/components/NewRoomForm/NewRoomForm.tsx
@@ -16,17 +16,19 @@ export function NewRoomForm() {
             })
     }, []);
     return (
-        <>
-            <h2>Choose a name and a voting system for your room</h2>
-            <form>
-                <input type="text" value={roomName} placeholder="Room's name" onChange={handleRoomNameChange} />
-                <select name="voting-system">
+        <div className="flex flex-col items-center justify-start sm:justify-center h-5/6">
+            <h2 className="mb-6 text-2xl">Choose a name and a voting system for your room</h2>
+            <form className="flex flex-col items-center justify-center gap-y-4">
+                <input type="text" value={roomName} placeholder="Room's name" onChange={handleRoomNameChange}
+                    className="w-full border-2 border-gray-200 focus:outline-indigo-500 rounded-md p-3" />
+                <select className="w-full bg-white p-3 border-2 border-gray-200 focus:border-indigo-500 rounded-md" name="voting-system">
                     {templates.map(template => {
-                        return <option value={template.title}>{templateToString(template)}</option>
+                        return <option key={template.title} value={template.title}>{templateToString(template)}</option>
                     })}
                 </select>
-                <button>Create room</button>
+                <input type="submit" value="Create room"
+                    className="w-full font-bold bg-indigo-500 hover:opacity-50 ease-in-out duration-200 rounded-md text-slate-50 p-3" />
             </form>
-        </>
+        </div>
     )
 }
